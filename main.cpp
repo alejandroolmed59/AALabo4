82e4ed97dddf71fd2e665a33007d552fb023194d
Alejandro Olmedo <00097017@uca.edu.sv>
2019-09-17 23:10:59 -0500

    Llegando a algo

--- main.cpp
+++ main.cpp
@@ -23,13 +23,13 @@ void printArray(int arr[], int size)
 
 int partition (int arr[], int low, int high)  
 {  
-    int pivot = arr[low];
-    int i = low;
-  
-    for (int j = low+1; j <= high; j++)  
+    int pivot = arr[high];
+    int i = low-1;
+
+    for (int j = low; j <= high-1; j++)  
     {   
-        if(arr[j]%2 !=0){
-          arr[j]= arr[j]-1;
+        if(arr[j]%2 !=0) {
+          arr[j]=arr[j]-1;
         }
         if (arr[j] < pivot)  
         {  
@@ -38,8 +38,9 @@ int partition (int arr[], int low, int high)
         }  
     }  
     
-    swap(&arr[i], &arr[low]);
-    return i;  
+    swap(&arr[i+1], &arr[high]);
+    
+    return i+1;  
 }  
   
 
@@ -50,15 +51,24 @@ void quickSort(int arr[], int low, int high)
         int pi = partition(arr, low, high);  
         quickSort(arr, low, pi - 1);  
         quickSort(arr, pi + 1, high);  
-    }  
+    }
+    if(arr[high]%2!=0) arr[high]-=1;
 }  
 
 
 int main()  
 {  
-    int arr[] = {6,1,8,5};  
+    int arr[] = {1,3,6,7};  
     int n = sizeof(arr) / sizeof(arr[0]);  
     quickSort(arr, 0, n - 1);  
     printArray(arr, n);  
+    int ant=0;
+    for(int i=0; i<n;i++){
+      if(arr[i]-ant>2){
+        arr[i]=arr[i]-2;
+      }
+      ant=arr[i];
+    }
+    printArray(arr, n);
     return 0;  
 }  
